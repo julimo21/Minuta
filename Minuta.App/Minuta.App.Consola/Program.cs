24bd68a503fbd2f625e0939c84c5ebf0c99ebfdb
Juliana Mora <111019132+julimo21@users.noreply.github.com>
2022-09-07 08:52:38 -0500

    Creación de IrepositorioVisitante y RepositorioVisitante

--- Minuta.App/Minuta.App.Consola/Program.cs
+++ Minuta.App/Minuta.App.Consola/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Minuta.App.Persistencia;
+using Minuta.App.Dominio;
+
+namespace Minuta.App.Consola
+{
+    internal class Program
+    {
+        private static IRepositorioVisitante _repoVisitante = new RepositorioVisitante(new Minuta.App.Persistencia.AppContext());
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hola para todos");            
+            //AddVisitante();
+            //BuscarVisitante(5);
+            //EliminarVisitante(6);
+            UpdateVisitante(1);
+            
+        }
+
+        private static void AddVisitante()
+        {
+            Console.WriteLine("Digite su nombre:");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Digite su apellido:");
+            string ape = Console.ReadLine();
+            Console.WriteLine("Digite su cedula:");
+            string ced = Console.ReadLine();
+            Console.WriteLine("Digite el numero de apartamento a visitar:");
+            string num = Console.ReadLine();
+            var visitante = new Visitante
+            {
+                nombre = nom,
+                apellidos = ape,
+                cedula = ced,
+                numApartamentoVis = num
+            };
+
+            _repoVisitante.AddVisitante(visitante);
+        }
+
+        private static void BuscarVisitante(int idv)
+        {
+            var visi = _repoVisitante.GetVisitante(idv);
+            Console.WriteLine("Visitante: "+ visi.nombre + " " + visi.apellidos + " C.C. " + visi.cedula);
+        }
+
+        private static void EliminarVisitante(int idv)
+        {
+            _repoVisitante.DeleteVisitante(idv);
+            Console.WriteLine("Visitante eliminado correctamente.");
+        }
+
+        private static void UpdateVisitante(int idv)
+        {
+            var visi = _repoVisitante.GetVisitante(idv);
+            if (visi != null)
+            {
+                
+            }
+            Console.WriteLine("Escriba su nombre:");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Escriba su apellido:");
+            string ape = Console.ReadLine();
+            Console.WriteLine("Escriba su cedula:");
+            string ced = Console.ReadLine();
+            Console.WriteLine("Digite el numero de apartamento a visitar:");
+            string num = Console.ReadLine();
+            /*var persona = new Persona
+            {
+                nombre = nom,
+                apellidos = ape,
+                cedula = ced
+            };
+
+            _repoPersona.AddPersona(persona);*/
+        }
+    }
+}
---
+++ Minuta.App/Minuta.App.Persistencia/AppRepositorios/RepositorioVisitante.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using Minuta.App.Dominio;
+
+namespace Minuta.App.Persistencia
+{
+    public class RepositorioVisitante:IRepositorioVisitante
+    {
+        private readonly AppContext appcox;
+
+        public RepositorioVisitante(AppContext appContext)
+        {
+            appcox = appContext;
+        }
+        IEnumerable<Visitante> IRepositorioVisitante.GetAllVisitante()
+        {
+            return appcox.vis;
+        }
+        Persona IRepositorioVisitante.AddVisitante(Minuta.App.Dominio.Visitante visitante)
+        {
+            var AgregarVisitante = appcox.vis.Add(visitante);
+            appcox.SaveChanges();
+
+            return AgregarVisitante.Entity;
+        }
+        Persona IRepositorioVisitante.GetVisitante(int idVisitante)
+        {
+            return appcox.vis.FirstOrDefault(v => v.id == idVisitante);
+        }
+        Persona IRepositorioVisitante.UpdateVisitante(Minuta.App.Dominio.Visitante visitante)
+        {
+            var VisitanteEncontrar = appcox.vis.FirstOrDefault(v => v.id == visitante.id);
+
+            if(VisitanteEncontrar != null)
+            {
+                VisitanteEncontrar.nombre = visitante.nombre;
+                VisitanteEncontrar.apellidos = visitante.apellidos;
+                VisitanteEncontrar.cedula = visitante.cedula;
+                VisitanteEncontrar.numApartamentoVis = visitante.numApartamentoVis;
+
+                appcox.SaveChanges();                
+            }
+            return VisitanteEncontrar;
+        }
+        void IRepositorioVisitante.DeleteVisitante(int idVisitante)
+        {
+            var VisitanteEncontrar = appcox.vis.FirstOrDefault(v => v.id == idVisitante);
+
+            if(VisitanteEncontrar == null)
+            {
+                return;
+            }
+
+            appcox.vis.Remove(VisitanteEncontrar);
+            appcox.SaveChanges();
+        }
+    }
+}
